3336afac27f56fa8902436bebf16e1164d3f0b38
Soton Ahmed <soton371@gmail.com>
2026-02-12 11:06:52 +0600

    feat: Add Redis integration with client initialization, shutdown, and test endpoints.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.core.settings import settings
 from app.db.health import router as db_health_router
 from app.modules.auth.router import router as auth_router
+from redis.asyncio import Redis
 
 app = FastAPI()
 
@@ -12,3 +13,27 @@ async def root():
 
 app.include_router(db_health_router)
 app.include_router(auth_router)
+
+
+# for redis
+@app.on_event("startup")
+async def startup():
+    app.state.redis = await Redis(host='localhost', port='6379')
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await app.state.redis.close()
+
+
+@app.get("/redis_set")
+async def redis_set():
+    await app.state.redis.set("test", "test")
+    return {"message": "test"}
+
+
+@app.get("/redis_get")
+async def redis_get():
+    result = await app.state.redis.get("test")
+    return {"message": result}
+
