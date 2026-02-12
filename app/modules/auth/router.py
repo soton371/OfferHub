acf4af24d9eeb468aa6231ddedbc995e5089fdf3
Soton Ahmed <soton371@gmail.com>
2026-02-12 10:04:48 +0600

    refactor: update user API routes by changing prefix from `/auth` to `/users` and simplifying endpoint paths.

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -3,25 +3,25 @@ from .schema import UserCreate, UserResponse, UserSearch, UserUpdate
 from .service import AuthServiceDep
 
 
-router = APIRouter(prefix="/auth", tags=["Auth"])
+router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
 async def create_user(data: UserCreate, service: AuthServiceDep):
     print(f'Router create_user data: {data}')
     return await service.create_user(data)
 
-@router.get("/users", response_model=list[UserResponse])
+@router.get("/", response_model=list[UserResponse])
 async def get_users(service: AuthServiceDep, search: UserSearch = Depends()):
     print(f'Router get_users search: {search}')
     return await service.get_users(search)
 
-@router.delete("/users/{id}", status_code=status.HTTP_200_OK)
+@router.delete("/{id}", status_code=status.HTTP_200_OK)
 async def delete_user(id: int, service: AuthServiceDep):
     print(f'Router delete_user data: {id}')
     result = await service.delete_user(id)
     return {"detail": f"User {result.name} deleted successfully"}
 
-@router.put("/users/{id}", status_code=status.HTTP_200_OK)
+@router.put("/{id}", status_code=status.HTTP_200_OK)
 async def update_user(id: int, data: UserUpdate, service: AuthServiceDep):
     print(f'Router update_user data: {id}')
     result = await service.update_user(id, data)
